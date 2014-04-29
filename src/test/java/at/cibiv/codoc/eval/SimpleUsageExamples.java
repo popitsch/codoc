caeb8ec48d3b115a4739ac015eb8d3044d89e2aa
niko <niko.popitsch@univie.ac.at>
2014-04-29 17:26:13 +0200

    fixed bug in usage examples

--- src/test/java/at/cibiv/codoc/eval/SimpleUsageExamples.java
+++ src/test/java/at/cibiv/codoc/eval/SimpleUsageExamples.java
@@ -2,8 +2,7 @@ package at.cibiv.codoc.eval;
 
 import java.io.File;
 import java.io.IOException;
-
-import org.broad.igv.variant.vcf.VCFVariant;
+import java.util.List;
 
 import at.cibiv.codoc.CompressedCoverageIterator;
 import at.cibiv.codoc.CoverageDecompressor;
@@ -23,81 +22,80 @@ import at.cibiv.ngs.tools.vcf.SimpleVCFVariant;
  */
 public class SimpleUsageExamples {
 
-    public static void iterateDOC(File covFile, File covVcfFile) throws CodocException, IOException {
+	public static void iterateDOC(File covFile, File covVcfFile) throws CodocException, IOException {
 
-	CoverageDecompressor cov = CoverageDecompressor.loadFromFile(covFile, covVcfFile);
-	try {
-	    CompressedCoverageIterator it = cov.getCoverageIterator();
-	    while (it.hasNext()) {
-		Float coverage = it.next();
-		GenomicPosition pos = it.getGenomicPosition();
-		System.out.println(pos.toString1based() + "\t" + coverage);
-	    }
-	} finally {
-	    cov.close();
+		CoverageDecompressor cov = CoverageDecompressor.loadFromFile(covFile, covVcfFile);
+		try {
+			CompressedCoverageIterator it = cov.getCoverageIterator();
+			while (it.hasNext()) {
+				Float coverage = it.next();
+				GenomicPosition pos = it.getGenomicPosition();
+				System.out.println(pos.toString1based() + "\t" + coverage);
+			}
+		} finally {
+			cov.close();
+		}
 	}
-    }
 
-    public static void queryDOC(File covFile, File covVcfFile) throws CodocException, IOException {
-	CoverageDecompressor cov = CoverageDecompressor.loadFromFile(covFile, covVcfFile);
-	try {
-	    GenomicPosition qpos = new GenomicPosition("20", 60070, COORD_TYPE.ONEBASED);
-	    CoverageHit hit = cov.query(qpos);
-	    if (hit != null) {
-		System.out.println(qpos.toString1based() + "\t" + hit.getInterpolatedCoverage() + " [" + hit.getLowerBoundary() + "/" + hit.getUpperBoundary()
-			+ "]");
-	    } else {
-		System.out.println("NO RESULT");
-	    }
-	} finally {
-	    cov.close();
+	public static void queryDOC(File covFile, File covVcfFile) throws CodocException, IOException {
+		CoverageDecompressor cov = CoverageDecompressor.loadFromFile(covFile, covVcfFile);
+		try {
+			GenomicPosition qpos = new GenomicPosition("20", 60070, COORD_TYPE.ONEBASED);
+			CoverageHit hit = cov.query(qpos);
+			if (hit != null) {
+				System.out.println(qpos.toString1based() + "\t" + hit.getInterpolatedCoverage() + " [" + hit.getLowerBoundary() + "/" + hit.getUpperBoundary()
+						+ "]");
+			} else {
+				System.out.println("NO RESULT");
+			}
+		} finally {
+			cov.close();
+		}
 	}
-    }
 
-    private static void querySNPs(File covFile, File covFile2, File covVcfFile) throws IOException, ParseException, CodocException {
-	SimpleVCFFile vcf = new SimpleVCFFile(covVcfFile);
-	CoverageDecompressor cov1 = CoverageDecompressor.loadFromFile(covFile, covVcfFile);
-	CoverageDecompressor cov2 = CoverageDecompressor.loadFromFile(covFile2, covVcfFile);
-	try {
-	    for (SimpleVCFVariant v : vcf.getVariants()) {
+	private static void querySNPs(File covFile, File covFile2, File covVcfFile) throws IOException, ParseException, CodocException {
+		SimpleVCFFile vcf = new SimpleVCFFile(covVcfFile);
+		CoverageDecompressor cov1 = CoverageDecompressor.loadFromFile(covFile, covVcfFile);
+		CoverageDecompressor cov2 = CoverageDecompressor.loadFromFile(covFile2, covVcfFile);
+		try {
+			for (SimpleVCFVariant v : vcf.getVariants()) {
 
-		CoverageHit h1 = cov1.query(v.getGenomicPosition());
-		int c1 = (h1 != null) ? h1.getRoundedCoverage() : 0;
-		CoverageHit h2 = cov1.query(v.getGenomicPosition());
-		int c2 = (h2 != null) ? h1.getRoundedCoverage() : 0;
-		System.out.println(v.getGenomicPosition().toString1based() + "\t" + v.getRefString() + "/" + v.getAltString() + "\t" + c1 + "\t" + c2);
-	    }
-	} finally {
-	    cov1.close();
-	    cov2.close();
+				CoverageHit h1 = cov1.query(v.getGenomicPosition());
+				int c1 = (h1 != null) ? h1.getRoundedCoverage() : 0;
+				CoverageHit h2 = cov2.query(v.getGenomicPosition());
+				int c2 = (h2 != null) ? h2.getRoundedCoverage() : 0;
+				System.out.println(v.getGenomicPosition().toString1based() + "\t" + v.getRefString() + "/" + v.getAltString() + "\t" + c1 + "\t" + c2);
+			}
+		} finally {
+			cov1.close();
+			cov2.close();
+		}
 	}
-    }
 
-    /**
-     * @param args
-     * @throws IOException
-     * @throws CodocException
-     * @throws ParseException
-     */
-    public static void main(String[] args) throws CodocException, IOException, ParseException {
-	File covFile = new File("src/test/resources/covcompress/small.compressed");
-	File covFile2 = new File("src/test/resources/covcompress/small.compressed");
-	File covVcfFile = new File("src/test/resources/covcompress/small.vcf");
 
-	 System.out
-	 .println("============================================================================");
-	 System.out
-	 .println("Iterate over all DOC values (use small CODOC files with this example code only!)");
-	 iterateDOC(covFile, covVcfFile);
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws CodocException
+	 * @throws ParseException
+	 */
+	public static void main(String[] args) throws CodocException, IOException, ParseException {
+		File covFile = new File("src/test/resources/covcompress/small.compressed");
+		File covFile2 = new File("src/test/resources/covcompress/small.compressed");
+		File covVcfFile = new File("src/test/resources/covcompress/small.vcf");
 
-	System.out.println("============================================================================");
-	System.out.println("Query a DOC value");
-	queryDOC(covFile, covVcfFile);
+		System.out.println("============================================================================");
+		System.out.println("Iterate over all DOC values (use small CODOC files with this example code only!)");
+		iterateDOC(covFile, covVcfFile);
 
-	System.out.println("============================================================================");
-	System.out.println("Calculate the coverage of a set of SNPs for a couple of CODOC files");
-	querySNPs(covFile, covFile2, covVcfFile);
+		System.out.println("============================================================================");
+		System.out.println("Query a DOC value");
+		queryDOC(covFile, covVcfFile);
 
-    }
+		System.out.println("============================================================================");
+		System.out.println("Calculate the coverage of a set of SNPs for a couple of CODOC files");
+		querySNPs(covFile, covFile2, covVcfFile);
+
+	}
 
 }
